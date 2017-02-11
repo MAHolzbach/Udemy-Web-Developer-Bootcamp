f325786f8dc6df2bf995d08542072e67edcdc75e
Mike <maholzbach@gmail.com>
2017-02-11 16:42:47 +0100

    JS Exercise

---
+++ JSStalker.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+
+var lastName = prompt("What is your last name?");
+
+var age = prompt("How old are you?");
+
+console.log("Hello " + firstName + lastName + ", how are you?");
+console.log(age + "? You're getting on in years.");
+console.log("You've been alive for " + (age*365) + "days.");
